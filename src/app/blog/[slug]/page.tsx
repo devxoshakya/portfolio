a86c577a55bdc4753dcc447802b1acf7f2089eb4
Dev Shakya <devxoshakya@gmail.com>
2024-08-12 12:56:15 +0530

    update blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -86,13 +86,14 @@ export default async function Blog({
       <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
         {post.metadata.title}
       </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+      <div className="flex justify-between items-center mt-2 mb-2 text-sm max-w-[650px]">
         <Suspense fallback={<p className="h-5" />}>
           <p className="text-sm text-neutral-600 dark:text-neutral-400">
             {formatDate(post.metadata.publishedAt)}
           </p>
         </Suspense>
       </div>
+      <div className="block w-[2.5rem] h-[0.2rem] mb-8 bg-[#ddd]"></div>
       <article
         className="prose dark:prose-invert"
         dangerouslySetInnerHTML={{ __html: post.source }}
